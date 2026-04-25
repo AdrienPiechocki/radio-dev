31f2c24fea5d7cc28e5b805a60dbc90959b9cba3
AdrienPiechocki <adrien.piechocki@proton.me>
2026-04-25 22:18:21 +0200

    fix cover

--- radio.sh
+++ radio.sh
@@ -12,7 +12,7 @@ cd "$SCRIPT_DIR"
 PLAYLIST_POS=0
 
 STATUS_JSON="./status${ICECAST_MOUNT//\//-}.json"
-COVER_ART="./web/cover.jpg"
+COVER_ART="./web/cover${ICECAST_MOUNT//\//-}.jpg"
 SCHEDULE_JSON="./schedule.json"
 LAST_EVENT_FILE="./.last_event"
 
